bcc7b42a141c4b6e54947cc0e4d2d6a173962a88
Yash Patil <49932152+yashpatils@users.noreply.github.com>
2026-02-09 07:41:27 -0800

    Add user and gym REST endpoints

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthController } from './auth.controller';
+import { GymsController } from './gyms/gyms.controller';
+import { GymsService } from './gyms/gyms.service';
+import { PrismaService } from './prisma.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  controllers: [AppController, AuthController],
+  controllers: [
+    AppController,
+    AuthController,
+    UsersController,
+    GymsController,
+  ],
+  providers: [PrismaService, UsersService, GymsService],
 })
 export class AppModule {}
