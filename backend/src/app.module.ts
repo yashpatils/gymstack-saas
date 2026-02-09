2df2d2ba33f5188593d9968f241cf69659f5696b
Yash Patil <49932152+yashpatils@users.noreply.github.com>
2026-02-09 07:31:05 -0800

    Add Prisma Neon connectivity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AppController, AuthController],
 })
 export class AppModule {}
